ce02dca2cfbabe7f7dab64da6489ac0677217cb8
sshukla456 <subhi25shukla@gmail.com>
2022-07-16 02:56:49 +0530

    Data taken

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,9 +1,46 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Styles/SignUp.css";
+import baseContext from "../Context/baseContext";
 
 export default function SignUp() {
+    const context = useContext(baseContext);
+    const SubmitData = async(e) => {
+        e.preventDefault();
+        const url = "http://localhost:5000/api/user/signup";
+        let Name = document.getElementById("NAME").value;
+        let Email = document.getElementById("EMAIL").value;
+        let Pass = document.getElementById("PASS").value;
+        let ConfPass = document.getElementById("CONFPASS").value;
+
+        if(ConfPass != Pass)
+        {
+            // context.showAlert("Enter Valid details");
+            // context.stopLoader();
+        }
+        else
+        {
+            let JsonObj = {
+                "name" : Name,
+                "email" : Email,
+                "password": Pass
+            }
+    
+            var resp = await fetch(url, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(JsonObj),
+              });
+            resp = await resp.json();
+            console.log(resp);
+        }  
+    };
+
+    
+    
     return (
         <div className="signup-full">
             <h1 className="main-head"> Welcome to Forms</h1>
@@ -19,6 +56,7 @@ export default function SignUp() {
                     type="text"
                     placeholder="Enter your Name"
                     className="signup-admin-inp"
+                    id = "NAME"
                     ></input>
                     </div>
 
@@ -28,6 +66,7 @@ export default function SignUp() {
                     type="email"
                     placeholder="Enter your email"
                     className="signup-admin-inp"
+                    id = "EMAIL"
                     ></input>
                     </div>
 
@@ -37,6 +76,7 @@ export default function SignUp() {
                     type="password"
                     className="signup-admin-inp"
                     placeholder="Enter your password here"
+                    id = "PASS"
                     ></input>
                     </div>
                     
@@ -46,9 +86,10 @@ export default function SignUp() {
                     type="password"
                     className="signup-admin-inp"
                     placeholder="Enter your password here"
+                    id = "CONFPASS"
                     ></input>
                     </div>
-                    <button className="admin-signup-btn">Sign Up</button> 
+                    <button className="admin-signup-btn" onClick={SubmitData}>Sign Up</button> 
                 </form>
 
                 <p className="signup-other">OR<br /> Sign Up using: </p>
